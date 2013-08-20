a480c069b10ceecee9dcbb39c3567fe1515bb9e7
wad3inthewater <wfulle20@gmail.com>
2013-08-20 08:03:06 -0400

    first push

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/**
+ * Parallax Scrolling Tutorial
+ * For NetTuts+
+ *  
+ * Author: Mohiuddin Parekh
+ *	http://www.mohi.me
+ * 	@mohiuddinparekh   
+ */
+
+
+$(document).ready(function(){
+	// Cache the Window object
+	$window = $(window);
+                
+   $('section[data-type="background"]').each(function(){
+     var $bgobj = $(this); // assigning the object
+                    
+      $(window).scroll(function() {
+                    
+		// Scroll the background at var speed
+		// the xPos is a negative value because we're scrolling it UP!								
+		var xPos = -($window.scrollLeft() / $bgobj.data('speed')); 
+		
+		// Put together our final background position
+		var coords = xPos + 'px 50% ';
+		console.log(coords);
+		console.log($bgobj);
+		// Move the background
+		$bgobj.css({ backgroundPosition: coords });
+		
+}); // window scroll Ends
+
+ });	
+
+}); 
+/* 
+ * Create HTML5 elements for IE's sake
+ */
+
+document.createElement("article");
+document.createElement("section");
