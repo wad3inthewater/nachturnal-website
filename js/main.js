a18ef0cc8cc5fd83a9e76b243c7152c3871f9255
wad3inthewater <wfulle20@gmail.com>
2013-12-09 02:10:32 -0500

    adding skrollr first look with images and testing data point animations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+/*$(function(){
+    var windowH = $(window).height();
+    var wrapperH = $('#wrapper').height();
+    if(windowH > wrapperH) {                            
+        $('#wrapper').css({'height':($(window).height())+'px'});
+    }                                                                               
+    $(window).resize(function(){
+        var windowH = $(window).height();
+        var wrapperH = $('#wrapper').height();
+        var differenceH = windowH - wrapperH;
+        var newH = wrapperH + differenceH;
+        var truecontentH = $('#truecontent').height();
+        if(windowH > truecontentH) {
+            $('#wrapper').css('height', (newH)+'px');
+        }
 
+    })          
+});*/
+
+$(document).ready(function() {
+
+function handleResize(){
+    $("section").height($(window).height());
+}
+
+handleResize();
+
+$(window).bind('resize', handleResize);
+});
